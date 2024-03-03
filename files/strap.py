04de001e1775e158939a3437de8098b6a2e45d20
Bill Sideris <bill88t@feline.gr>
2024-03-03 04:05:32 +0200

    Initial add

--- files/strap.py
+++ files/strap.py
@@ -1,6 +1,4 @@
-print("""
-    This script is run during the jpkgstrapping of the package from a linux host.
-    
-    You should copy the files you want in the system from here.
-    You should NOT bother with the manifest or the uninstall files from here.
-""")
+for i in ["sensors.lja", "sensors.py"]:
+    shutil.copy(i, path.join(root, "bin", i))
+
+shutil.copy("sensors.man", path.join(root, "usr/share/man", "sensors.man"))
--- makefile
+++ makefile
@@ -12,4 +12,4 @@ modules:
 package: modules
 	@python3 -u scripts/generate_package.py
 clean:
-	@if [ -e "package.jpk" ]; then rm package.jpk; fi
+	@if [ -e "lm_sensors.jpk" ]; then rm lm_sensors.jpk; fi
