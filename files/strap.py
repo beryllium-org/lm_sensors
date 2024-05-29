f43d00a81d36f056a3b0fd927f494fd4851b8d58
Bill Sideris <bill88t@feline.gr>
2024-05-29 03:20:44 +0300

    copyfile

--- files/strap.py
+++ files/strap.py
@@ -1,4 +1,4 @@
 for i in ["sensors.lja", "sensors.py"]:
-    shutil.copy(i, path.join(root, "bin", i))
+    shutil.copyfile(i, path.join(root, "bin", i))
 
-shutil.copy("sensors.man", path.join(root, "usr/share/man", "sensors.man"))
+shutil.copyfile("sensors.man", path.join(root, "usr/share/man", "sensors.man"))
