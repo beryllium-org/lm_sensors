dac62362c1732e57a043095efdda8b5e9bda28a5
Bill Sideris <bill88t@feline.gr>
2024-06-06 17:10:36 +0300

    Add battery support

--- files/sensors.py
+++ files/sensors.py
@@ -3,12 +3,20 @@ vr("no", True)
 if cpu.temperature is not None:
     vr("no", False)
     term.write(
-        "cpu_thermal\nAdapter: Cpu device\ntemp1: +" + str(cpu.temperature)[:5] + "°C"
+        "cpu_thermal\nAdapter: Cpu device\ntemp1: +" + str(cpu.temperature)[:5] + "°C\n"
     )
 
 if cpu.voltage is not None:
     vr("no", False)
     term.write("voltage1: " + str(cpu.voltage) + "V")
+
+if "bat" in be.devices.keys():
+    vr("no", False)
+    for pv[get_pid()]["i"] in be.devices["bat"].keys():
+        term.write("bat" + str(vr("i")))
+        term.write("Adapter: kernel")
+        term.write("inV: " + str(be.devices["bat"][vr("i")].voltage) + "V")
+        term.write("Charge: " + str(be.devices["bat"][vr("i")].percentage) + "%\n")
 
 if vr("no"):
     term.write("No sensors detected!")
