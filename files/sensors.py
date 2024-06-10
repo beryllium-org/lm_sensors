36289936dc44e7826c4d4e71e8405e0c54159580
Bill Sideris <bill88t@feline.gr>
2024-06-10 17:16:23 +0300

    Update sensors

--- files/sensors.py
+++ files/sensors.py
@@ -1,14 +1,19 @@
 rename_process("sensors")
 vr("no", True)
-if cpu.temperature is not None:
+pass
+vr("temp", cpu.temperature)
+for _ in range(10):
+    pass
+
+if vr("temp") is not None:
     vr("no", False)
     term.write(
-        "cpu_thermal\nAdapter: Cpu device\ntemp1: +" + str(cpu.temperature)[:5] + "°C\n"
+        "cpu_thermal\nAdapter: Cpu device\ntemp1: +" + str(vr("temp"))[:5] + "°C\n"
     )
 
 if cpu.voltage is not None:
     vr("no", False)
-    term.write("voltage1: " + str(cpu.voltage) + "V")
+    term.write("cpu_voltage\nAdapter: Cpu device\ninV: " + str(cpu.voltage) + "V\n")
 
 if "bat" in be.devices.keys():
     vr("no", False)
