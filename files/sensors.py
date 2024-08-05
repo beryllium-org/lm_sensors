03f48d608acfe11824ea7ace5e944d281d4d88b9
Bill Sideris <bill88t@feline.gr>
2024-08-06 01:28:47 +0300

    More battery properties probed

--- files/sensors.py
+++ files/sensors.py
@@ -11,6 +11,11 @@ if vr("temp") is not None:
         "cpu_thermal\nAdapter: Cpu device\ntemp1: +" + str(vr("temp"))[:5] + "°C\n"
     )
 
+if "temp" in be.devices.keys():
+    vr("no", False)
+    for pv[get_pid()]["i"] in be.devices["temp"].keys():
+        term.write(vr("i").name + "\nAdapter: kernel\ntemp1:" + str(vr("i").temperature) + "°C\n")
+
 if cpu.voltage is not None:
     vr("no", False)
     term.write("cpu_voltage\nAdapter: Cpu device\ninV: " + str(cpu.voltage) + "V\n")
@@ -18,10 +23,16 @@ if cpu.voltage is not None:
 if "bat" in be.devices.keys():
     vr("no", False)
     for pv[get_pid()]["i"] in be.devices["bat"].keys():
+        vr("cdev", be.devices["bat"][vr("i")])
         term.write("bat" + str(vr("i")))
         term.write("Adapter: kernel")
-        term.write("inV: " + str(be.devices["bat"][vr("i")].voltage) + "V")
-        term.write("Charge: " + str(be.devices["bat"][vr("i")].percentage) + "%\n")
+        term.write("inV: " + str(vr("cdev").voltage) + "V")
+        term.write("Charge: " + str(vr("cdev").percentage) + "%")
+        if hasattr(vr("cdev"), "status"):
+            term.write("Status: " + vr("cdev").status)
+        if hasattr(vr("cdev"), "charging_enabled"):
+            term.write("Charging enabled: " + str(vr("cdev").charging_enabled))
+        term.write()
 
 if vr("no"):
     term.write("No sensors detected!")
